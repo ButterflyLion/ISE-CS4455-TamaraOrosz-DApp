54765db4f7c043bfda73f92cb4004ed7fed019d4
ButterflyLion <orosztama@gmail.com>
2024-05-03 22:04:17 +0100

    removed starter pages and assets and added header and footer

---
+++ src/components/layout/FooterLayout.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { FacebookFilled } from '@vicons/material'
+import { InstagramFilled } from '@vicons/antd'
+import { NIcon } from 'naive-ui'
+</script>
+
+<template>
+  <footer className="footer">
+    <div>
+      <a href="/">
+        <img alt="Logo" class="logo" src="@/assets/logo.svg" width="80" />
+      </a>
+      <p style="text-align: center">© Tamara Orosz, 2024. All rights reserved.</p>
+    </div>
+    <div class="footer-section">
+      <p>Info</p>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+      <a href="/news">News</a>
+    </div>
+    <div class="footer-section">
+      <p>Help</p>
+      <a href="/faqs">FAQs</a>
+      <a href="/help">Customer Support</a>
+      <a href="/privacy-policy">Privacy Policy</a>
+      <a href="/terms-of-service">Terms of Service</a>
+    </div>
+    <div>
+      <p>Follow us on social media</p>
+      <n-icon size="30" color="#FE15C6">
+        <FacebookFilled />
+        <InstagramFilled />
+      </n-icon>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  display: flex;
+  justify-content: space-around;
+  background-color: #ccebff;
+  align-self: end;
+  width: 100%;
+  padding: 20px;
+  text-align: center;
+}
+
+.footer p {
+  color: #01328a;
+  text-decoration: none;
+  text-transform: uppercase;
+  padding: 0px 15px 0px 15px;
+  text-align: left;
+}
+
+.footer a {
+  font-size: medium;
+  text-decoration: none;
+  padding: 0px 15px 0px 15px;
+  text-align: left;
+}
+
+.footer-section {
+  display: flex;
+  flex-direction: column;
+}
+
+@media (hover: hover) {
+  a:hover {
+    color: #FE15C6;
+  }
+}
+</style>
---
+++ src/components/layout/HeaderLayout.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+    <div class="full-width-header">
+      <nav>
+        <RouterLink to="/">
+          <img alt="Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        </RouterLink>
+      </nav>
+      <nav class="pages-links">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about-us">About us</RouterLink>
+        <RouterLink to="/event">Event</RouterLink>
+        <RouterLink to="/create-a-wallet">Wallets</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.full-width-header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background-color: #CCEBFF;
+  padding: 1rem 2rem;
+}
+
+.pages-links {
+  margin-left: auto;
+  margin-right: 2rem;
+}
+
+nav {
+  text-transform: uppercase;
+}
+
+nav a {
+  padding: 14px 16px;
+  text-align: center;
+  font-size: 20px;
+  margin-right: 5px;
+}
+
+@media (hover: hover) {
+  a:hover {
+    color: #FE15C6;
+  }
+}
+</style>
