42254b7e458a9f2c5e82fde2c8b7b94a5983635a
ButterflyLion <orosztama@gmail.com>
2024-05-18 16:59:41 +0100

    added wallet creation and keystore download

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ISE-CS4455-TamaraOrosz-DApp
 A Web3 DApp that implements simple ticketing system using the Ethereum Sepolia Testnet as the blockchain for Solidity smart contract deployments, and Vue.js for front end.
+
+# Colours in logo:
+- Background: #CCEBFF
+- Icon: #FE15C6
+- Text: #01328A
--- src/views/CreateWalletView.vue
+++ src/views/CreateWalletView.vue
@@ -1,10 +1,99 @@
-<script></script>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Web3 from 'web3'
+
+export default defineComponent({
+  setup() {
+    const password = ref('')
+    const walletAddress = ref('')
+    const privKey = ref('')
+    const keystore = ref('')
+
+    const createWallet = async () => {
+      if (password.value === '') {
+        alert('Please enter a password')
+        return
+      }
+
+      const web3 = new Web3("https://rpc2.sepolia.org")
+      const wallet = web3.eth.accounts.create()
+
+      walletAddress.value = wallet.address
+      privKey.value = wallet.privateKey
+
+      const newKeystore = await web3.eth.accounts.encrypt(wallet.privateKey, password.value)
+      console.log(newKeystore)
+      keystore.value = JSON.stringify(newKeystore)
+      console.log(keystore.value)
+    }
+
+    const downloadKeystore = () => {
+      if(keystore.value === '') {
+        alert('Please create a wallet first')
+        return
+      }
+      const keystoreBlob = new Blob([keystore.value], { type: 'text/plain;charset=utf-8' })
+      saveAs(keystoreBlob, walletAddress.value + '.json')
+    }
+
+    const saveAs = (blob: Blob | MediaSource, filename: string) => {
+      const url = URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = filename
+      a.click()
+      URL.revokeObjectURL(url)
+    }
+
+    return {
+      password,
+      walletAddress,
+      privKey,
+      keystore,
+      createWallet,
+      downloadKeystore
+    }
+  }
+})
+</script>
 
 <template>
   <main>
-    <h1>Welcome!</h1>
-    <n-card class="card" style="color: #01328a">
-      <n-input type="password" show-password-on="click" placeholder="Set your wallet password" :maxlength="20" name="passwordInput" />
+    <n-card title="Create a wallet" class="card">
+      <div class="inputGroup">
+        <n-input-group>
+          <n-input
+            v-model:value="password"
+            type="password"
+            show-password-on="click"
+            placeholder="Set your wallet password"
+            :minlength="12"
+          />
+          <n-button @click="createWallet" type="primary" color="#fe15c6"> Create wallet </n-button>
+        </n-input-group>
+        <n-flex justify="space-around" :style="{ padding: '10px' }">
+          <n-input
+            v-model:value="walletAddress"
+            type="text"
+            placeholder="Wallet Address will be displayed here"
+            :disabled="true"
+          />
+          <n-input
+            v-model:value="privKey"
+            type="text"
+            placeholder="Private key will be displayed here"
+            :disabled="true"
+          />
+          <n-input
+            v-model:value="keystore"
+            type="textarea"
+            :autosize="true"
+            placeholder="Key Store will be displayed here"
+            :disabled="true"
+          />
+        </n-flex>
+          <n-button @click="downloadKeystore" type="primary" color="#fe15c6">Download</n-button>
+      </div>
     </n-card>
   </main>
 </template>
@@ -24,9 +113,11 @@ main {
   background-color: #ccebff;
   text-align: center;
   text-transform: uppercase;
-  color: #01328a;
+  height: fit-content;
+  width: auto;
 }
-.card title {
-  color: #01328a;
+
+.inputGroup {
+  text-transform: none;
 }
 </style>
